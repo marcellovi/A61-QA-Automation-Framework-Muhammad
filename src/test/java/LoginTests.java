35367c0f57ad3abc199d3250066775ed65c2ddcc
Marcello F. Vieira <marcello.strategy@gmail.com>
2024-07-19 19:34:39 -0500

    Creating a New Branch Homework-15

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -20,5 +20,6 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+        // Creating a New Branch named "homework-15"
     }
 }
