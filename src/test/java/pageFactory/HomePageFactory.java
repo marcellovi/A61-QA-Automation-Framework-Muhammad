858be0b90673bedac5755174e1d2b487717e43ad
Marcello F. Vieira <marcello.strategy@gmail.com>
2024-08-18 15:06:29 -0500

    Refactor & Added New Tests
    Added Test to Create and Delete Playlist.
    Added Priority to create a correct flow of test ( Create, Rename and Delete )

--- src/test/java/pageFactory/HomePageFactory.java
+++ src/test/java/pageFactory/HomePageFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePageFactory extends BasePageFactory {
 
@@ -29,6 +30,21 @@ public class HomePageFactory extends BasePageFactory {
     @FindBy( xpath = "//div[contains(text(),'My New Playlist')]")
     WebElement newPlaylistConfirmMsg;
 
+    @FindBy( xpath = "//nav[@class='menu playlist-item-menu']//ul//li[contains(text(),'Delete')]")
+    WebElement deletePopUpMenuOption;
+
+    @FindBy( xpath = "//div[contains(text(),'My Playlist')]")
+    WebElement myPlaylistConfirmMsg;
+
+    @FindBy(css="i[data-testid=\"sidebar-create-playlist-btn\"]")
+    WebElement btnPlusIcon;
+
+    @FindBy(css="li[data-testid=\"playlist-context-menu-create-simple\"")
+    WebElement liAddNewPlaylist;
+
+    @FindBy(xpath="//input[@name=\"name\"]")
+    WebElement txtPlaylist;
+
 
     public WebElement getUserAvatar(){
         return avatarIcon;
@@ -39,19 +55,49 @@ public class HomePageFactory extends BasePageFactory {
         return this;
     }
 
+    public HomePageFactory rightClickNewPlaylist(){
+        actions.contextClick(myNewPlalistOption).perform();
+        return this;
+    }
+
     public HomePageFactory doubleClickPlayList() {
         actions.doubleClick(myPlalistOption).perform();
         return this;
     }
 
-    public HomePageFactory renamePlaylistField(String text) {
-        playlistEditField.sendKeys(Keys.chord(Keys.CONTROL,"a"),text);
+    public HomePageFactory clickDeleteMenuOption() {
+        deletePopUpMenuOption.click();
+        return this;
+    }
+
+    public HomePageFactory renamePlaylistField(String playlistName) {
+        playlistEditField.sendKeys(Keys.chord(Keys.CONTROL,"a"),playlistName);
         playlistEditField.sendKeys(Keys.ENTER);
         return this;
     }
 
-    public Boolean isNewPlayListDisplayed() {
-        return newPlaylistConfirmMsg.isDisplayed();
+    public WebElement isNewPlayListDisplayed() {
+        return wait.until(ExpectedConditions.visibilityOf(newPlaylistConfirmMsg));
+    }
+
+    public WebElement isMyPlayListDisplayed() {
+        return wait.until(ExpectedConditions.visibilityOf(myPlaylistConfirmMsg));
+    }
+
+    public HomePageFactory clickAddPlayListIcon(){
+        btnPlusIcon.click();
+        return this;
+    }
+
+    public HomePageFactory clickMenuNewPlayList(){
+        liAddNewPlaylist.click();
+        return this;
+    }
+
+    public HomePageFactory addNamePlaylistField(String playlistName) {
+        txtPlaylist.sendKeys(Keys.chord(Keys.CONTROL,"a"),playlistName);
+        txtPlaylist.sendKeys(Keys.ENTER);
+        return this;
     }
 
 }
